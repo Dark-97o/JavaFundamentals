948b362448ddbcc612e2d04cda6830fa96bc38c7
Subh97-0 <114503763+Dark-97o@users.noreply.github.com>
2025-02-08 19:23:34 +0530

    update

---
+++ armstrong.java
@@ -0,0 +1,18 @@
+public class armstrong {
+    public static void main(String[] args) {
+        int n = 153;
+        int sum = 0;
+        int temp = n;
+        while (temp > 0) {
+            int rem = temp % 10;
+            sum += rem * rem * rem;
+            temp /= 10;
+        }
+        if (n == sum) {
+            System.out.println(n + " is an Armstrong number.");
+        } else {
+            System.out.println(n + " is not an Armstrong number.");
+        }
+    }
+    
+}
